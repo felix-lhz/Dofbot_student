e0c328e0672179ddda36ca7328de76d1aad4cd6d
felix-lhz <1175906605@qq.com>
2024-10-22 11:03:01 +0800

    black format success

--- dh_kine_demo.py
+++ dh_kine_demo.py
@@ -2,23 +2,23 @@ import roboticstoolbox as rtb
 import numpy as np
 import math
 
-pi = 3.1415926          # 定义pi常数
+pi = math.pi  # 定义pi常数
 
-l1 = 0.1045             # 定义第一连杆长度
-l2 = 0.08285            # 定义第三连杆长度
-l3 = 0.08285            # 定义第四连杆长度
-l4 = 0.12842            # 定义第五连杆长度
+l1 = 0.1045  # 定义第一连杆长度
+l2 = 0.08285  # 定义第三连杆长度
+l3 = 0.08285  # 定义第四连杆长度
+l4 = 0.12842  # 定义第五连杆长度
 
 # student version
 # 用改进DH参数发表示机器人正运动学
 # TODO: modify the dh param
 dofbot = rtb.DHRobot(
     [
-        rtb.RevoluteMDH(a = 1, alpha=11, d=111, offset=1111),
-        rtb.RevoluteMDH(),
-        rtb.RevoluteMDH(),
-        rtb.RevoluteMDH(),
-        rtb.RevoluteMDH()
+        rtb.RevoluteMDH(d=l1),
+        rtb.RevoluteMDH(alpha=-pi / 2, offset=-pi / 2),
+        rtb.RevoluteMDH(a=l2),
+        rtb.RevoluteMDH(a=l3, offset=pi / 2),
+        rtb.RevoluteMDH(alpha=pi / 2, d=l4),
     ]
 )
 
@@ -26,30 +26,42 @@ dofbot = rtb.DHRobot(
 print(dofbot)
 
 
-'''
+"""
 Part1 给出一下关节姿态时的机械臂正运动学解，并附上仿真结果
 0.(demo) [0., pi/3, pi/4, pi/5, 0.]
 1.[pi/2, pi/5, pi/5, pi/5, pi]
 2.[pi/3, pi/4, -pi/3, -pi/4, pi/2]
 3.[-pi/2, pi/3, -pi/3*2, pi/3, pi/3]
-'''
+"""
 
 # part1 demo
-fkine_input0 = [0., pi/3, pi/4, pi/5, 0.]
+fkine_input0 = [0.0, pi / 3, pi / 4, pi / 5, 0.0]
 fkine_result0 = dofbot.fkine(fkine_input0)
-print(fkine_result0)
+print("part1-demo: " + str(fkine_result0))
 dofbot.plot(q=fkine_input0, block=True)
 
 # part1-1
+fkine_input1 = [pi / 2, pi / 5, pi / 5, pi / 5, pi]
+fkine_result1 = dofbot.fkine(fkine_input1)
+print("part1-1: " + str(fkine_result1))
+dofbot.plot(q=fkine_input1, block=True)
 
 # part1-2
+fkine_input2 = [pi / 3, pi / 4, -pi / 3, -pi / 4, pi / 2]
+fkine_result2 = dofbot.fkine(fkine_input2)
+print("part1-2: " + str(fkine_result2))
+dofbot.plot(q=fkine_input2, block=True)
 
 # part1-3
+fkine_input3 = [-pi / 2, pi / 3, -pi / 3 * 2, pi / 3, pi / 3]
+fkine_result3 = dofbot.fkine(fkine_input3)
+print("part1-1: " + str(fkine_result3))
+dofbot.plot(q=fkine_input3, block=True)
 
 
-'''
+"""
 Part1 给出一下关节姿态时的机械臂逆运动学解，并附上仿真结果
-0.(demo) 
+0.(demo)
     [
         [-1., 0., 0., 0.1,],
         [0., 1., 0., 0.],
@@ -77,24 +89,79 @@ Part1 给出一下关节姿态时的机械臂逆运动学解，并附上仿真�
         [0., -0.866, 0.5, 0.3073],
         [0., 0., 0., 1.]
     ]
-'''
+"""
 
-#part2 demo
-target_pos0 = np.array([
-    [-1., 0., 0., 0.1,],
-    [0., 1., 0., 0.],
-    [0., 0., -1., -0.1],
-    [0., 0., 0., 1.]
-])
+# part2 demo
+target_pos0 = np.array(
+    [
+        [
+            -1.0,
+            0.0,
+            0.0,
+            0.1,
+        ],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, -1.0, -0.1],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
 ikine_result0 = dofbot.ik_LM(target_pos0)[0]
-print("ikine: ", np.array(ikine_result0))
+print("ikine-demo: ", np.array(ikine_result0))
 dofbot.plot(q=ikine_result0, block=True)
 
 
-#part2-1
+# part2-1
+target_pos1 = np.array(
+    [
+        [
+            1.0,
+            0.0,
+            0.0,
+            0.1,
+        ],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.1],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
+ikine_result1 = dofbot.ik_LM(target_pos1)[0]
+print("ikine-1: ", np.array(ikine_result1))
+dofbot.plot(q=ikine_result1, block=True)
 
 
-#part2-2
+# part2-2
+target_pos2 = np.array(
+    [
+        [
+            math.cos(pi / 3),
+            0.0,
+            -math.sin(pi / 3),
+            0.05,
+        ],
+        [0.0, 1.0, 0.0, 0.03],
+        [math.sin(pi / 3), 0.0, math.cos(pi / 3), -0.1],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
+ikine_result2 = dofbot.ik_LM(target_pos2)[0]
+print("ikine-2: ", np.array(ikine_result2))
+dofbot.plot(q=ikine_result2, block=True)
 
 
-#part2-3
+# part2-3
+target_pos3 = np.array(
+    [
+        [
+            -0.866,
+            -0.25,
+            -0.433,
+            -0.03704,
+        ],
+        [0.5, -0.433, -0.75, -0.06415],
+        [0.0, -0.866, 0.5, 0.3073],
+        [0.0, 0.0, 0.0, 1.0],
+    ]
+)
+ikine_result3 = dofbot.ik_LM(target_pos3)[0]
+print("ikine-3: ", np.array(ikine_result3))
+dofbot.plot(q=ikine_result3, block=True)
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 from dofbot import DofbotEnv
 import numpy as np
 import time
@@ -9,17 +7,16 @@ import time
 import math
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     env = DofbotEnv()
     env.reset()
     Reward = False
 
-
-    '''
+    """
     constants here
-    '''
-    GRIPPER_DEFAULT_ANGLE = 20. / 180. * 3.1415
-    GRIPPER_CLOSE_ANGLE = -20. / 180. * 3.1415
+    """
+    GRIPPER_DEFAULT_ANGLE = 20.0 / 180.0 * 3.1415
+    GRIPPER_CLOSE_ANGLE = -20.0 / 180.0 * 3.1415
 
     # define state machine
     INITIAL_STATE = 0
@@ -30,8 +27,7 @@ if __name__ == '__main__':
     BACK_STATE = 5
     current_state = INITIAL_STATE
 
-
-    initial_jointposes = [1.57, 0., 1.57, 1.57, 1.57]
+    initial_jointposes = [1.57, 0.0, 1.57, 1.57, 1.57]
 
     # offset to grasp object
     obj_offset = [-0.023, -0.023, 0.09]
@@ -44,15 +40,12 @@ if __name__ == '__main__':
 
     while not Reward:
 
-
-
-        '''
+        """
         #获取物块位姿、目标位置和机械臂位姿，计算机器臂关节和夹爪角度，使得机械臂夹取绿色物块，放置到紫色区域。
-        '''
+        """
 
-        '''
+        """
         code here
-        '''
-
+        """
 
         Reward = env.reward()
